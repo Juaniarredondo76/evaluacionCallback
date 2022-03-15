95dac862ad0027c9f69cfef6079e19e781b94e94
401 <401@academicos.cesde.edu.co>
2022-03-15 11:34:44 -0500

    NTG003 se crean y se intenta desarrollar el problema 3 de la evalucion

--- problema3.js
+++ problema3.js
@@ -14,3 +14,23 @@ callback debe permitir entregar la sumatoria de niveles de
 energía entregados por los alimentos vegetales consumidos en
 la dieta de Grogu.
 */
+
+let nombreAlimentos=['cucaracha rostizada','Atascaburras','Bienmesabe','Chochos','Ajoatao','Morteruelo','Patatas a la importancia','Sopa boba','Engañamaridos','Japuta']
+let tipoAlimento=['vegetal','animal','insecto']
+let energia=[200,150,220,100.110]
+
+
+let comidas=[]
+for(let i=0; i<50; i++){
+
+    let plato={}
+
+    //crear un atributo 
+
+    comidas.nombre=(nombreAlimentos[Math.floor(Math.random()*nombreAlimentos.length)])
+    comidas.tipo=(tipoAlimento[Math.floor(Math.random()*tipoAlimento.length)])
+    comidas.energia=(energia[Math.floor(Math.random()*energia.length)])
+
+   comidas.push(plato)
+}
+ 
